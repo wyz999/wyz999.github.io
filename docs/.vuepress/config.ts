72af4efc118734e5fc9c8c1e9bde66950e5c1cb4
卓越 <1207848000@qq.com>
2025-08-19 14:54:39 +0800

    feat(site): deploy to root domain  set vuepress base to '/'

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/zhuo.github.io/',
+  base: '/',
   lang: 'zh-CN',
   locales: {
     '/': {
